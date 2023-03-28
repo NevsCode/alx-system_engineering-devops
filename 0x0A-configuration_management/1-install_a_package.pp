df26d98d1c01b27223d0cd541e23933360eaebb5
Neville Mpiana <nevillempiana0619@gmail.com>
2023-03-28 11:23:24 -0700

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install puppet-lint using Puppet
-
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
