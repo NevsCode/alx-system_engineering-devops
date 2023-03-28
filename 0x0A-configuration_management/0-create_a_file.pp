a9b86b98bcf9f6224472710bc900da8a86349517
Neville Mpiana <nevillempiana0619@gmail.com>
2023-03-28 11:21:41 -0700

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,9 @@
-# Create a file in /tmp using Puppet
+# Create file in /tmp
 
-file { 'holberton':
-  ensure  => present,
-  path    => '/tmp/holberton',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
-}
-
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
